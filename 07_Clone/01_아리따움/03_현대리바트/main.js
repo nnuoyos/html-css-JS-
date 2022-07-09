ac192ffb1cccdd6519509beaca64ca5c20ba1cbd
so young <104718503+nnuoyos@users.noreply.github.com>
2022-07-09 20:59:01 +0900

    클론코딩 현대리바트 메인영역/Js 작업중

---
+++ 07_Clone/01_아리따움/03_현대리바트/main.js
@@ -0,0 +1,11 @@
+$('.mainSlide').slick({
+    prevArrow: '.mainSlide .prev_btn',
+    nextArrow: '.mainSlide .next_btn',
+    //dots: true,
+    //appendDots: '.mainSlide .msPager',
+    autoplay: true,
+    autoplaySpeed: 8000,
+    speed: 1000,
+    fade: true,
+});
+
